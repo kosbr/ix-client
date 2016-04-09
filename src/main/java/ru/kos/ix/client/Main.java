052669fc06ae289ff5308417503f440d3224e848
Konstantin <bryazginkos@yahoo.com>
2016-04-09 09:21:04 +0600

    simple test

--- src/main/java/ru/kos/ix/client/Main.java
+++ src/main/java/ru/kos/ix/client/Main.java
@@ -1,12 +1,12 @@
 package ru.kos.ix.client;
 
+import ru.kos.ix.dto.AnsTask;
 import ru.kos.ix.dto.Task;
 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
+
 
 /**
  * Created by Константин on 08.04.2016.
@@ -18,38 +18,20 @@ public class Main {
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(s.getOutputStream());
         ObjectInputStream objectInputStream = new ObjectInputStream(s.getInputStream());
 
-        List<String> stringList = new ArrayList<String>();
-        stringList.add("Hello");
-        stringList.add("World");
-
         Task task = new Task();
         Object[] arguments = new Object[1];
-        arguments[0] = stringList;
+        arguments[0] = "Some string";
         task.setArguments(arguments);
         task.setId(1);
-        task.setMethodName("method1");
-        task.setServiceName("service");
+        task.setMethodName("toUpper");
+        task.setServiceName("some");
 
 
         objectOutputStream.writeObject(task);
         objectOutputStream.flush();
 
-        Task task2 = new Task();
-        Object[] arguments2 = new Object[1];
-        arguments[0] = stringList;
-        task2.setArguments(arguments2);
-        task2.setId(2);
-        task2.setMethodName("method2");
-        task2.setServiceName("service2");
-
-        objectOutputStream.writeObject(task2);
-        objectOutputStream.flush();
-
-        Object data = objectInputStream.readObject();
+        AnsTask data = (AnsTask)objectInputStream.readObject();
         System.out.println("data=" + data);
-
-        Object data2 = objectInputStream.readObject();
-        System.out.println("data2=" + data2);
 
         s.close();
     }
